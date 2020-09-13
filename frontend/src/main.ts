a97ef2b2bd65c11ba5ae985a283acb6f27d0e682
bewr <bw@shoutrlabs.com>
2020-09-13 15:09:56 +0200

    refactor and cleanup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,19 +1,16 @@
 import Vue from "vue";
 import VueCompositionAPI from "@vue/composition-api";
+import { messages, defaultLocale } from "@/i18n";
 import PortalVue from "portal-vue";
 import Fragment from "vue-fragment";
 import VueI18n from "vue-i18n";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import vuetify from "./plugins/vuetify";
-import { messages, defaultLocale } from "@/i18n";
+import Vuetify from "vuetify/lib";
+
+Vue.use(Vuetify);
 Vue.use(VueI18n);
-const i18n = new VueI18n({
-  messages,
-  locale: defaultLocale,
-  fallbackLocale: defaultLocale
-});
 
 Vue.use(VueCompositionAPI);
 Vue.use(PortalVue);
@@ -21,10 +18,15 @@ Vue.use(Fragment.Plugin);
 
 Vue.config.productionTip = false;
 
+const i18n = new VueI18n({
+  messages,
+  locale: defaultLocale,
+  fallbackLocale: defaultLocale
+});
 new Vue({
   router,
   store,
   i18n,
-  vuetify,
+  vuetify: new Vuetify({}),
   render: h => h(App)
 }).$mount("#app");
